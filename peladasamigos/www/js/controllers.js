7be45640132942a0ca60527f7a581a0d7653dae6
leobo <leobororo@gmail.com>
2016-12-04 17:43:37 -0200

    alterações para desabilitar horários já reservados e para enviar apenas os horarios que o usuario selecionou e nao os que já estavam selecionados

--- peladasamigos/www/js/controllers.js
+++ peladasamigos/www/js/controllers.js
@@ -1,30 +1,75 @@
 angular.module('starter.controllers', ['ngCordova'])
 
+.controller('HomeCtrl', function ($scope) {
 
-.controller('HomeCtrl', function ($scope) {})
-
+})
 
 .controller('ProductsCtrl', function ($scope, $http, Products) {
     Products.getProductsAll().success(function (data) {
-        console.log(data);
+        console.log("Get produtos executado com sucesso");
         $scope.products = data;
     }).error(function (data, status) {
-        console.log("Erro ao executar get");
+        console.log("Erro ao executar get produtos");
     });
 })
 
-.controller('ProductServicesCrtl', function ($scope, $stateParams, Products) {
+.controller('ProductServicesCrtl', function ($scope, $stateParams, $ionicPopup, Products) {
 
 
     Products.getProduct($stateParams.productId).success(function (data) {
-        console.log(data);
+        console.log("Get produto executado com sucesso");
+
+        var reservaParaAlteracao = [
+            {
+                hour: data.reservas[0].hour,
+                select: data.reservas[0].select,
+                selectBD: data.reservas[0].select
+            },
+            {
+                hour: data.reservas[1].hour,
+                select: data.reservas[1].select,
+                selectBD: data.reservas[1].select
+            },
+            {
+                hour: data.reservas[2].hour,
+                select: data.reservas[2].select,
+                selectBD: data.reservas[2].select
+            },
+            {
+                hour: data.reservas[3].hour,
+                select: data.reservas[3].select,
+                selectBD: data.reservas[3].select
+            },
+            {
+                hour: data.reservas[4].hour,
+                select: data.reservas[4].select,
+                selectBD: data.reservas[4].select
+            },
+            {
+                hour: data.reservas[5].hour,
+                select: data.reservas[5].select,
+                selectBD: data.reservas[5].select
+            },
+            {
+                hour: data.reservas[6].hour,
+                select: data.reservas[6].select,
+                selectBD: data.reservas[6].select
+            },
+            {
+                hour: data.reservas[7].hour,
+                select: data.reservas[7].select,
+                selectBD: data.reservas[7].select
+            }
+        ]
+
+        data.reservas = reservaParaAlteracao;
+
         $scope.product = data;
     }).error(function (data, status) {
-        console.log("Erro ao executar get");
+        console.log("Erro ao executar get produto");
     });
 
     $scope.fazerPedidoReserva = function (quadra) {
-
         var push = PushNotification.init(
         {
             "android": { "senderID": "747034740221" },
@@ -37,16 +82,61 @@ angular.module('starter.controllers', ['ngCordova'])
         });
 
         push.on('registration', function (data) {
+
+            var quadraParaPost = {
+                _id: quadra._id,
+                name: quadra.name,
+                business_adress: quadra.business_adress,
+                ico: quadra.ico,
+                reservas: [
+                    {
+                        hour: quadra.reservas[0].hour,
+                        select: quadra.reservas[0].select && !quadra.reservas[0].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[1].hour,
+                        select: quadra.reservas[1].select && !quadra.reservas[1].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[2].hour,
+                        select: quadra.reservas[2].select && !quadra.reservas[2].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[3].hour,
+                        select: quadra.reservas[3].select && !quadra.reservas[3].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[4].hour,
+                        select: quadra.reservas[4].select && !quadra.reservas[4].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[5].hour,
+                        select: quadra.reservas[5].select && !quadra.reservas[5].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[6].hour,
+                        select: quadra.reservas[6].select && !quadra.reservas[6].selectBD
+                    },
+                    {
+                        hour: quadra.reservas[7].hour,
+                        select: quadra.reservas[7].select && !quadra.reservas[7].selectBD
+                    }
+                ]
+            }
+
             var pedidoReservaQuadra = {
                 deviceId: data.registrationId,
-                quadra: quadra
+                quadra: quadraParaPost
             }
 
             Products.postPedidoProduct(pedidoReservaQuadra).success(function (data) {
-                console.log(data);
-                $scope.product = data;
+                console.log("Post enviado com sucesso");
+                $ionicPopup.alert({
+                    title: 'Pedido de reserva enviado com sucesso!'
+                });
+
             }).error(function (data, status) {
-                console.log("Erro ao executar get");
+                console.log("Erro ao executar post");
             });
         });
 
